2c1d4fe39317d2eea329782ee35679847c09c887
Andrzej Korbiel <andrzej.korbiel@Lumesse.com>
2019-11-13 14:18:07 +0100

    Basic structure pt 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,36 @@
 getData = () => {
   const $app = document.getElementById("app");
-  $app.innerHTML = `<div></div>`;
 
-  fetch("http://localhost:8080/abc", { mode: "no-cors" })
+
+  fetch("http://localhost:8080/search", { mode: "no-cors" })
     .then(res => {
       return res.json();
     })
-    .then(myJSON => console.log(myJSON))
+    .then(myJSON => {
+        console.log(myJSON)
+        for (let i = 0; i < myJSON.length; i++) {
+            let userName = myJSON[i].user.name,
+                profilePhoto = myJSON[i].user.profile_image_url_https,
+                followersCount = myJSON[i].user.followers_count,
+                userDesc = myJSON[i].user.description,
+                date = myJSON[i].created_at,
+                fullText = myJSON[i].full_text,
+                favourites = myJSON[i].favorite_count,
+                imgSrc = myJSON[i].extended_entities != undefined ? myJSON[i].extended_entities.media[0].media_url : '',
+                imgs = imgSrc != '' ? `<img src=${imgSrc} />` : ``;
+
+            $app.innerHTML += `<div class="singleT">
+                <h1>${userName}<img src=${profilePhoto} /></h1>
+                <p>${followersCount}</p>
+                <h2>${userDesc}</h2>
+                <p>${date}</p>
+                <p>${fullText}</p>
+                <p><span class="favorite">${favourites}</span></p>
+                <div class="images"> 
+                ${imgs}    
+                </div>
+                </div>`;
+        }
+    })
     .catch(error => console.error("Error:", error));
 };
--- src/server.js
+++ src/server.js
@@ -8,15 +8,8 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
 
-const searchQuery = { q: "node.js", count: 2 };
-let x = '';
-
-app.get("/abc", function(req, res) {
-  res.json(x);
-});
-
+/* Twitter API */
 const Twitter = require("twitter");
-
 const client = new Twitter({
   consumer_key: "a6gfUYI6R4SmMsyBkPD4jxCvb",
   consumer_secret: "KV80YBip7MkWM6UsPw3lhPe5DBlQjpdUPzt66FrwzYqFQns8Z9",
@@ -24,13 +17,16 @@ const client = new Twitter({
   access_token_secret: "plaBaN0cNECRRxY9tg7jASzmB5aEy7GRd0f4mXBpgXlGD",
 });
 
+const searchQuery = { q: "", count: 20, lang: "pl", result_type: "recent", from: "MorawieckiM", tweet_mode: "extended"};
+let dataFromTwitter = [];
+
 function getTweets(error, tweets, response) {
-  let tweetsText = tweets.statuses;
-  for (let i = 0; i < tweetsText.length; i++) {
-    x += tweetsText[i].text;
-  }
+  dataFromTwitter = tweets.statuses;
 }
 
 client.get("search/tweets", searchQuery, getTweets);
 
-
+/* Routes */
+app.get("/search", function(req, res) {
+    res.json(dataFromTwitter);
+});
