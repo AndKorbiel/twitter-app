08e86eebb24f1dcbfbd7b9a048a073978aaa28c4
Andrzej Korbiel <andrzej.korbiel@Lumesse.com>
2019-11-13 09:17:30 +0100

    Basic structure

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,8 @@ getData = () => {
   const $app = document.getElementById("app");
   $app.innerHTML = `<div></div>`;
 
-  fetch("http://localhost:8080/", { mode: "no-cors" })
+  fetch("http://localhost:8080/abc", { mode: "no-cors" })
     .then(res => {
-      console.log(res);
       return res.json();
     })
     .then(myJSON => console.log(myJSON))
--- src/js/server.js
+++ src/js/server.js
@@ -4,40 +4,33 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
-app.get("/", function(req, res) {
-  client.get("search/tweets", searchQuery, getTweets);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+const searchQuery = { q: "node.js", count: 2 };
+let x = '';
+
+app.get("/abc", function(req, res) {
   res.json(x);
 });
 
-app.get("/abc", function(req, res) {
-  res.json({ foo: "bar" });
-});
+const Twitter = require("twitter");
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
-
-var Twitter = require("twitter");
-
-var client = new Twitter({
+const client = new Twitter({
   consumer_key: "a6gfUYI6R4SmMsyBkPD4jxCvb",
   consumer_secret: "KV80YBip7MkWM6UsPw3lhPe5DBlQjpdUPzt66FrwzYqFQns8Z9",
   access_token_key: "1194209758322057217-fjtZ4YWwc7s8GYyAKQXKtdsT1F3gpx",
   access_token_secret: "plaBaN0cNECRRxY9tg7jASzmB5aEy7GRd0f4mXBpgXlGD",
-  request_options: {
-    cors: "no-cors"
-  }
 });
 
-var x = "";
-const searchQuery = { q: "node.js", count: 2 };
 function getTweets(error, tweets, response) {
   let tweetsText = tweets.statuses;
   for (let i = 0; i < tweetsText.length; i++) {
-    x = tweetsText[i].text;
+    x += tweetsText[i].text;
   }
 }
 
-// function getTweetsRes(error, tweets, response) {
-//   return response.body;
-// }
+client.get("search/tweets", searchQuery, getTweets);
+
+
